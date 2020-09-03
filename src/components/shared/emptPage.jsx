ed89d8e28c99231c9f6dc95ab38b125004968fe2
fadilmuh22 <fadilmuh2002@gmail.com>
2020-09-03 15:22:37 +0700

    fix Navbar still floating when ModalError appears

--- src/components/shared/emptPage.jsx
+++ src/components/shared/emptPage.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import ModalError from './ModalError';
 
+// ? Notes dari fadil: Penamaan Component pakai PascalCase, sama itu typo harusnya 'Empty'
 class emptPage extends Component {
+
+    state = {
+        displayModal: true,
+    }
+    
+    setDisplayModal = (status) => {
+        this.setState({
+            displayModal: status,
+        });
+    }
+
     render() {
         return (
             <>
+            {this.state.displayModal ? <ModalError onClose = {() => this.setDisplayModal(false)} /> : null}
             <Navbar />
-            <div className="emptpage">
+            <div className="emptpage" onClick = {() => this.setDisplayModal(true)} >
                 <img src={"https://res.cloudinary.com/codelifings/image/upload/v1599049238/ok_diqja0.png"} />
             </div>
             </>
