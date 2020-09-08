ca9e54f250fabff48d8b54c5f462d72c6471d90a
Kazenyaru <fadilmuh2002@gmail.com>
2020-09-08 13:17:57 +0700

    topup style adjustment and dropdown functional on Lihat Lainnya

--- src/components/topup/DetailTransaction.jsx
+++ src/components/topup/DetailTransaction.jsx
@@ -5,6 +5,17 @@ import arrowback from "../../images/arrowback.svg";
 import logo from "../../images/logo.svg";
 
 class DetailTransaction extends Component {
+
+    state = {
+        isInvoice: false,
+    }
+
+    setIsInvoice = (status) => {
+        this.setState({
+            isInvoice: status
+        })
+    }
+
     render() {
         return (
             <div className="all-forms-style detail-transaction">
@@ -19,55 +30,68 @@ class DetailTransaction extends Component {
                 <div className="logo-invest">
                     <img src={logo} alt="" />
                 </div>
-                <p className="title">Top Up</p>
+                <p className="title">{this.state.isInvoice ? "INVOICE" : "DETAIL TRANSAKSI"}</p>
 
                 <div className="payment-detail box-form-data">
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Tanggal</p>
                         <p className="amount">22/09/2020</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">No Invoice</p>
                         <p className="amount">ABC123-EF45</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Status</p>
-                        <p className="amount">Pending</p>
+                        <p className={"amount " + (this.state.isInvoice ? "text-blue" : "")} >{this.state.isInvoice ? "Success" : "Pending"}</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Pembayaran</p>
                         <p className="amount">Top Up Wallet</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Metode Pengiriman</p>
                         <p className="amount">BCA Virtual Account</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Nominal Top Up</p>
                         <p className="amount text-blue">Rp. 200,000,-</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Administrasi</p>
                         <p className="amount text-blue">Rp. 2,500,-</p>
                     </div>
-                    <div class="tile-active d-flex justify-content-between">
+                    <div className="tile-active d-flex justify-content-between">
                         <p className="name">Total</p>
                         <div>
                             <br />
-                            <h3 className="amount text-blue">Rp. 202, 500</h3>
+                            <p className="amount h3 text-blue">Rp. 202, 500</p>
                         </div>
                     </div>
                     <br />
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Batas Waktu</p>
                         <p className="amount">12;34;00</p>
                     </div>
 
                     <br />
 
-                    <p className="note">*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error reiciendis culpa quidem. Aliquam labore totam ea, fuga tenetur fugiat obcaecati eveniet itaque quisquam, hic magni neque, aperiam unde accusamus harum.</p>
+                    {this.state.isInvoice ?
+                        <div>
+                            <p className="note">*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error reiciendis culpa quidem. Aliquam labore totam ea, fuga tenetur fugiat obcaecati eveniet itaque quisquam, hic magni neque, aperiam unde accusamus harum.</p>
 
-                    <button className="but-solid" type="submit">BAYAR TRANSAKSI</button>
+                            <div className="d-flex justify-content-between">
+                                <button className="but">BUTUH BANTUAN</button>
+                                <button className="but-solid" type="submit" >KIRIM BUKTI</button>
+                            </div>
+
+                        </div>
+                        :
+                        <div>
+                            <p className="note">*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error reiciendis culpa quidem. Aliquam labore totam ea, fuga tenetur fugiat obcaecati eveniet itaque quisquam, hic magni neque, aperiam unde accusamus harum.</p>
+                            <button className="but-solid" type="submit" onClick={() => this.setIsInvoice(true)}>BAYAR TRANSAKSI</button>
+                        </div>
+                    }
 
                 </div>
 
--- src/components/topup/MethodTopUp.jsx
+++ src/components/topup/MethodTopUp.jsx
@@ -4,8 +4,110 @@ import { Link } from "react-router-dom";
 import arrowback from "../../images/arrowback.svg";
 import logo from "../../images/logo.svg";
 
-class MethodTopUp extends Component {
+
+import PropTypes from 'prop-types';
+import { Slide } from "react-reveal";
+
+
+class PaymentMethod extends Component {
     render() {
+        const { methods } = this.props;
+        return (
+            <>
+                {
+                    methods.map((method, i) => (
+                        <div key={i}>
+                            <div className="d-flex justify-content-between">
+                                <div className="title-alt">{method.title}</div>
+                                {methods.length === 1 ?
+                                    (
+                                        <span className="more-transfer-method" onClick={method.toggle}>
+                                            Lihat Lainnya<i className="fas fa-chevron-down ml-2"></i>
+                                        </span>
+                                    ) : <div></div>
+                                }
+                            </div>
+                            <p className="subtitle">{method.subtitle}</p>
+                            {method.logos.map((value, j) =>
+                                <div className="radio-bank" key={j}>
+                                    <input
+                                        type="radio"
+                                        name="bank"
+                                    />
+
+                                    <img src={value} alt="bca" />
+                                    <br />
+                                </div>
+                            )}
+                            <br />
+                        </div>
+                    )
+                    )
+                }
+            </>
+        );
+    }
+}
+
+
+PaymentMethod.propTypes = {
+    methods: PropTypes.arrayOf(
+        PropTypes.exact({
+            first: PropTypes.bool,
+            toggle: PropTypes.func,
+            title: PropTypes.string,
+            subtitle: PropTypes.string,
+            logos: PropTypes.arrayOf(PropTypes.string),
+        })
+    ).isRequired,
+};
+
+
+
+
+class MethodTopUp extends Component {
+    state = {
+        toggleMethods: false,
+    }
+
+    setToggleMethods = () => {
+        this.setState({
+            toggleMethods: !(this.state.toggleMethods),
+        })
+    }
+
+    render() {
+        const methods = [
+            {
+                first: true,
+                toggle: this.setToggleMethods,
+                title: 'VIRTUAL ACCOUNT',
+                subtitle: 'Dengan Kode Unik Semua Jadi Cepat',
+                logos: [
+                    logo,
+                    logo,
+                    logo,
+                ]
+            },
+            {
+                title: 'VIRTUAL ACCOUNT1',
+                subtitle: 'Dengan Kode Unik Semua Jadi Cepat',
+                logos: [
+                    logo,
+                    logo,
+                    logo,
+                ]
+            },
+            {
+                title: 'VIRTUAL ACCOUNT2',
+                subtitle: 'Dengan Kode Unik Semua Jadi Cepat',
+                logos: [
+                    logo,
+                    logo,
+                    logo,
+                ]
+            },
+        ]
         return (
             <div className="all-forms-style">
                 <div className="bg">
@@ -27,48 +129,34 @@ class MethodTopUp extends Component {
                         <input
                             type="number"
                             name="username"
-                            // disabled
-                            // ref={ i => this.username = i}
-                            id=""
                         />
                     </div>
                 </div>
 
                 <p className="box-form-title">Pilih Metode Pembayaran</p>
-                <div className="box-form-data">
-
-                    <div class="d-flex justify-content-between">
-                        <div className="title-alt">Transfer Rekening</div>
-                        <p className="more-transfer-method">
-                            Lihat Lainnya
-                        </p>
-                    </div>
-                    <div className="radio-bank checked">
-                        <input
-                            type="radio"
-                            name="bank"
-                            // disabled
-                            // ref={ i => this.username = i}
-                            id=""
-                        />
-
-                        <img src={logo} alt="bca" />
-                    </div>
+                <div className="payment-methods box-form-data">
+                    {
+                        this.state.toggleMethods ?
+                            <Slide bottom>
+                                <PaymentMethod methods={methods} />
+                            </Slide>
+                            : <PaymentMethod methods={[methods[0]]} />
+                    }
                 </div>
 
                 <p className="box-form-title">Ringkasan Pembayaran</p>
                 <div className="payment-detail box-form-data">
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Harga</p>
                         <p className="amount">1.000.000</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Biaya Admin</p>
                         <p className="amount">+1.000</p>
                     </div>
-                    <div class="d-flex justify-content-between">
+                    <div className="d-flex justify-content-between">
                         <p className="name">Total Pembayaran</p>
-                        <p className="amount">1,001.000</p>
+                        <p className="amount h4">1,001.000</p>
                     </div>
                 </div>
 
