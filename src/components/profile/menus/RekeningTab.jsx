22c43d678e7ec2f287f2ee3ae6a7c635128b033e
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2020-09-28 23:50:49 +0700

    add mdoal fill bank

--- src/components/profile/menus/RekeningTab.jsx
+++ src/components/profile/menus/RekeningTab.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import reknull from '../../../images/profile/reknull.svg'
 import {Button} from '@material-ui/core';
+import ModalTemplate from '../../shared/ModalTemplate';
+import ModalFillBank from './ModalFillBank'
+
+
 
 
 class RekeningTab extends Component {
+    state ={
+        modalAddBank : true
+    }
+
+    closeMod = ()=> this.setState({modalAddBank : false})
+    openMod = ()=> this.setState({modalAddBank : true})
+
     render() {
         return (
             <div className='rektab'>
+                <ModalTemplate 
+                    onOpen={this.state.modalAddBank} 
+                    onClose={this.closeMod}
+                    component ={()=>ModalFillBank(this.closeMod, this.openMod)}
+                />
                 <img src={reknull} alt="rekening null"/>
                 <p className="desc">
                 Anda belum menyimpan Rekening Bank. <br/>
                 Untuk kemudahan pembayaran kartu kredit / debit, simpan informasi kartu kredit / debit Anda.
                 </p>
-                <Button>Tambah Kartu Kredit / Debit</Button>
+                <Button onClick={this.openMod}>Tambah Kartu Kredit / Debit</Button>
             </div>
         );
     }
