9271d2a16a98a92d256d539a4331effe92a5a880
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2020-10-07 23:11:58 +0700

    responsive navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import HamburgerMenu from 'react-hamburger-menu'
+import HamburgerMenu from 'react-hamburger-menu'
 import { connect } from 'react-redux';
 
 import logo from '../../images/logo.svg'
@@ -7,10 +7,12 @@ import { Link, NavLink } from 'react-router-dom'
 import { Button, ClickAwayListener } from "@material-ui/core";
 import kuki from '../../helpers/cookie'
 import walletnav from '../../images/walletnav.svg'
+import ModalTemplate from './ModalTemplate';
 
 class Navbar extends Component {
     state={
         isOpen : false,
+        isOpenRes : false,
         statusId : null,
     }
     componentDidMount(){
@@ -34,14 +36,58 @@ class Navbar extends Component {
        this.props.changeTab(val)
     } 
 
+    handleClickRes = (val)=>{
+        this.setState({isOpenRes : false})
+        this.props.changeTab(val)
+     } 
+
+    commpMenuList = ()=>(
+        <>
+        <div className="boxsaldo">
+            <img src={walletnav} alt="saldo"/>
+            <p className="nominal">Saldo  <br/> <span>Rp 1500000</span> </p>
+            <Button>
+            <i className="fas fa-plus-circle"></i> Top Up
+            </Button>
+        </div>
+        <div className='menuslistpop'>
+            <a href='/'>Notifications</a>
+            <a href='/' className='border-bottom'>Invite Friends</a>
+            <Link to='/profile'  onClick={()=> this.handleClick('profile')}>Profile Saya</Link>
+            <Link to='/profile'  onClick={()=> this.handleClick('business')}>Bisnis Saya</Link>
+            <Link to='/profile'  onClick={()=> this.handleClick('history')}>History</Link>
+            <Link to='/profile'  onClick={()=> this.handleClick('favorite')}>Favorit</Link>
+            <Link to='/profile'  onClick={()=> this.handleClick('list')}>List of Investment</Link>
+            <Link to='/profile'  onClick={()=> this.handleClick('dividen')} className='border-bottom'>Dividen</Link>
+            <a href='/'>Settings</a>
+            <a href='/' style={{cursor : 'pointer', color:'#4CB5EF', }}
+            onClick={()=>{
+                kuki.remove('auth')
+                kuki.remove('status')
+                kuki.remove('token')
+                kuki.remove('full_name')
+                kuki.remove('email')
+                kuki.remove('phone_number')
+            }}> <b>Logout</b></a>
+        </div>
+        </>
+    )
+
+    responsiveMenuComp = ()=>(
+        <div className='menus-pop-res'>
+            {this.commpMenuList()}
+        </div>
+    )
+
     render() {
         return (
             <div>
+                <ModalTemplate onOpen={this.state.isOpenRes} onClose={()=> this.setState({isOpenRes : !this.state.isOpenRes})} component={this.responsiveMenuComp} />
                   <nav>
                     <div className="left">
-                    {/* <HamburgerMenu
-                        isOpen={this.state.open}
-                        menuClicked={()=> this.setState({open : !this.state.open})}
+                    <HamburgerMenu
+                        isOpen={this.state.isOpenRes}
+                        menuClicked={()=> this.setState({isOpenRes : !this.state.isOpenRes})}
                         width={17}
                         height={12}
                         strokeWidth={1}
@@ -50,7 +96,7 @@ class Navbar extends Component {
                         borderRadius={0}
                         animationDuration={0.3}
                         className='burger'
-                    /> */}
+                    />
                     <Link to='/'>
                         <img src={logo} alt="logo"/>
                     </Link>
@@ -66,44 +112,14 @@ class Navbar extends Component {
                             <li> <Link to='/'>FAQ</Link> </li>
                             {
                                kuki.get('auth') ? 
-                                <li className='popovercus'>
-                                    <ClickAwayListener onClickAway={()=>this.setState({isOpen : false})}>
+                                <li className=''>
+                                    <ClickAwayListener className='awaylistener' onClickAway={()=>this.setState({isOpen : false})}>
                                         <div className='popovercus'>
                                             <Link to='/profile' className='linktoprof'>
                                                 <img className='ava' src='https://pbs.twimg.com/profile_images/1108355467888259072/gxh4yKYO.png' alt="ava"/> <span>{kuki.get('full_name')}</span>  
                                             </Link>
                                             <i onClick={this.handlePop} className="ml-3 fas fa-chevron-down" style={{color: '#A5A5A5'}}></i>
-                                            {this.state.isOpen ? (
-                                            <div className='menus-pop'>
-                                                <div className="boxsaldo">
-                                                    <img src={walletnav} alt="saldo"/>
-                                                    <p className="nominal">Saldo  <br/> <span>Rp 1500000</span> </p>
-                                                    <Button>
-                                                    <i className="fas fa-plus-circle"></i> Top Up
-                                                    </Button>
-                                                </div>
-                                                <div className='menuslistpop'>
-                                                    <a href='/'>Notifications</a>
-                                                    <a href='/' className='border-bottom'>Invite Friends</a>
-                                                    <Link to='/profile'  onClick={()=> this.handleClick('profile')}>Profile Saya</Link>
-                                                    <Link to='/profile'  onClick={()=> this.handleClick('business')}>Bisnis Saya</Link>
-                                                    <Link to='/profile'  onClick={()=> this.handleClick('history')}>History</Link>
-                                                    <Link to='/profile'  onClick={()=> this.handleClick('favorite')}>Favorit</Link>
-                                                    <Link to='/profile'  onClick={()=> this.handleClick('list')}>List of Investment</Link>
-                                                    <Link to='/profile'  onClick={()=> this.handleClick('dividen')} className='border-bottom'>Dividen</Link>
-                                                    <a href='/'>Settings</a>
-                                                    <a href='/' style={{cursor : 'pointer', color:'#4CB5EF', }}
-                                                    onClick={()=>{
-                                                        kuki.remove('auth')
-                                                        kuki.remove('status')
-                                                        kuki.remove('token')
-                                                        kuki.remove('full_name')
-                                                        kuki.remove('email')
-                                                        kuki.remove('phone_number')
-                                                    }}> <b>Logout</b></a>
-                                                </div>
-                                            </div>
-                                            ) : null}
+                                            {this.state.isOpen ?<div className='menus-pop'>{this.commpMenuList()}</div>  : null}
                                         </div>
                                     </ClickAwayListener>
                                 </li>
