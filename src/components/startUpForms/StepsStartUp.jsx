2d81bdb85c7362cc1cdb6a8b1f3dd52f5124bad9
iqbal <muhammadiqbal.rpl@gmail.com>
2020-10-19 18:24:14 +0700

    responsive register startup

--- src/components/startUpForms/StepsStartUp.jsx
+++ src/components/startUpForms/StepsStartUp.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class StepsStartUp extends Component {
     render() {
@@ -10,20 +10,42 @@ class StepsStartUp extends Component {
             'Informasi Non Finansial',
             'Media',
         ]
+
+        const active = this.props.active
+        const whereStep = steps[active - 1]
+        const percentage = steps.map((res, i) => active === i + 1 ? active * 16.6 : null)
+        const finalPercentage = Math.round(percentage[active - 1])
+
         return (
-            <div className="steps-startup">
-                <ul>
-                    {
-                        steps.map((res,i)=>
-                        <li key={i} className={this.props.active === i+1? 'actbread' : ''}>
-                            <div className="number">{i +1}</div> 
-                            <span>{res}</span>
-                        </li>
-                        )
-                    }
-                </ul>
-                <hr/>
-            </div>
+            <Fragment>
+                <div className="steps-startup">
+                    <ul>
+                        {
+                            steps.map((res,i)=>
+                            <li key={i} className={this.props.active === i+1? 'actbread' : ''}>
+                                <div className="number">{i +1}</div> 
+                                <span>{res}</span>
+                            </li>
+                            )
+                        }
+                    </ul>
+                    <hr/>
+                </div>
+                
+                <div className="steps-startup-mobile">
+                    <div class={`c100 p${finalPercentage} small`}>
+                        <span>{finalPercentage}%</span>
+                        <div class="slice">
+                            <div class="bar"></div>
+                            <div class="fill"></div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div style={{ fontSize: '14px' }}>{whereStep}</div>
+                    </div>
+                </div>
+            </Fragment>
         );
     }
 }
