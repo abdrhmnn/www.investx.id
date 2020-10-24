1134b7d847c9a82d7907274e5a437f2f325ea334
nisacodelifings <annisap.siswa@smkn4bdg.sch.id>
2020-10-24 05:17:35 -0700

    add wallet fix screen respnsv

--- src/components/withDraw/Wallet.jsx
+++ src/components/withDraw/Wallet.jsx
@@ -10,6 +10,18 @@ import topup from "../../images/withdraw/topup.svg";
 
 class Wallet extends Component {
   render() {
+    const styleSaran = {
+      display: 'inline-block',
+      border: 'none',
+      marginTop: '20px',
+      marginRight: '20px',
+      paddingLeft: '20px',
+      paddingRight: '20px',
+      background: '#DEDEDE',
+      fontSize: '9pt',
+      whiteSpace: 'nowrap'
+    }
+
     const jumlahSaran = [
       "20.000.000",
       "30.000.000",
@@ -20,33 +32,9 @@ class Wallet extends Component {
     ];
 
     const listSaran = jumlahSaran.map((saran) => {
-      return <Button>{saran}</Button>;
+      return <Button style={styleSaran}>{saran}</Button>;
     });
 
-    // const butBlue = {
-    //   marginBottom: "10px",
-    //   width: "197px",
-    //   height: "36px",
-    //   fontSize: "14px",
-    //   textTransform: "uppercase",
-    //   color: "#ffffff",
-    //   background: "#01579b",
-    //   borderRadius: "8px",
-    //   border: "none",
-    //   outline: "none",
-    // };
-    // const but = {
-    //   width: "100px",
-    //   width: "197px",
-    //   height: "36px",
-    //   fontSize: "14px",
-    //   color: "#0288d1",
-    //   borderRadius: "8px",
-    //   border: "1px solid #0288d1",
-    //   backgroundColor: "white",
-    //   fontSize: "16px",
-    //   outline: "unset !important",
-    // };
     return (
       <div className="all-forms-style">
         <div className="bg">
@@ -101,165 +89,103 @@ class Wallet extends Component {
                     <Button className='line-blue'>TOP UP</Button>
                   </div>
                 </div>
+                <div className="list-saran">{listSaran}</div>
+              </div>
+
+              <div className="box-right mt-4">
+               <div className="title-alt riwayat-trans">Riwayat Transaksi</div> <br />
+                <div className="menu">
+                  <a
+                    style={{
+                      background: "#4CB5EF",
+                      color: "white",
+                    }}
+                  >
+                    Semua
+                  </a>
+                  <a>Pembelian</a>
+                  <a>Top Up</a>
+                <a>With Draw</a>
+                </div>
+
+                <div className="box-isi">
+                  
+                  <div className="kotak berhasil">
+                    
+                    <p className="tanggal-now">10 Aug 2020 | 08:30</p><br />
+                     <div className="row">
+                        <div className="col-md-8 input-nominal">
+                          <p className="title-transaksi">No Transaksi</p>
+                          <p className="no-transaksi">
+                           (IVR/204151525/XX/VII/3333222626)
+                          </p>
+                        </div>
+                        <div className="col-md total-pemb">
+                           <p className="title-transaksi">Total Pembayaran <span>Rp 20.000.000</span></p>
+                          <p className="no-transaksi">
+                          Lihat Detail Transaksi
+                          </p>
+                      </div>
+                    </div>
+
+                   <div className="detail-kotak row mt-2">
+                      <div className="col-md-4">
+                      <img src={topup} className="mt-3" alt="" />
+                      <span className="title-nominal">Top Up</span>
+                      <p className="jumlah-nominal">+20.000.000</p>
+
+                        </div>
+                    <div className="col-md-4 total-pemb">
+                      <span className="status-nom"> Status</span>
+                      <p className="status-">Transaksi Berhasil</p>
+                        </div>
+                    <div className="col-md">
+                       <Button className='topup-but'>TOP UP</Button>
+                        </div>
+                    </div>
+                  </div>
+
+                  {/* kotak batal */}
+                  <div className="kotak batal">
+                    
+                    <p className="tanggal-now">10 Aug 2020 | 08:30</p><br />
+                     <div className="row">
+                        <div className="col-md-8 input-nominal">
+                          <p className="title-transaksi">No Transaksi</p>
+                          <p className="no-transaksi">
+                           (IVR/204151525/XX/VII/3333222626)
+                          </p>
+                        </div>
+                        <div className="col-md total-pemb">
+                           <p className="title-transaksi">Total Pembayaran <span>Rp 20.000.000</span></p>
+                          <p className="no-transaksi">
+                          Lihat Detail Transaksi
+                          </p>
+                      </div>
+                    </div>
+
+                   <div className="detail-kotak row mt-2">
+                      <div className="col-md-4">
+                      <img src={topup} className="mt-3" alt="" />
+                      <span className="title-nominal">Top Up</span>
+                      <p className="jumlah-nominal">+20.000.000</p>
+
+                        </div>
+                    <div className="col-md-4 total-pemb">
+                      <span className="status-nom"> Status</span>
+                      <p className="status-">Transaksi Berhasil</p>
+                        </div>
+                    <div className="col-md">
+                       <Button className='topup-but'>TOP UP</Button>
+                        </div>
+                    </div>
+                  </div>
+                  
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="box saldo">
-          <img className="wallet" src={wallet} alt="" />
-          <span className="total-saldo">Total Saldo</span>
-          <div className="uang jumlah-uang">
-            Rp. <span className="jumlah">20.000.000</span>
-          </div>
-
-          <div className="wallet-bawah">
-            <div className="kiri uang-saldo-rupiah">
-              <div className="uang uang-jumlah-saldo">
-                <span className="jumlah" style={{ fontWeight: "400" }}>
-                  Saldo Rupiah
-                </span>
-              </div>
-
-              <div className="uang saldo-black">
-                Rp. <span className="jumlah">15.000.000</span>
-              </div>
-            </div>
-
-            <div className="kiri uang-ku">
-              <div className="uang ku-ada">
-                <span className="jumlah" style={{ fontWeight: "400" }}>
-                  Total Rupiah
-                </span>
-              </div>
-
-              <div className="uang ku-kan">
-                Rp. <span className="jumlah">5.000.000</span>
-              </div>
-            </div>
-            <br />
-
-            <div className="know-more uang-lagi">
-              <span className="jumlah" style={{ fontWeight: "400" }}>
-                Pelajari Lebih Lanjut
-              </span>
-              <a href="#" style={{ color: "#01579B", fontWeight: "500" }}>
-                {" "}
-                disini
-              </a>
-            </div>
-          </div>
-<<<<<<< HEAD
-        </div>
-        
-        <div className="box nominal">
-=======
-        </div> */}
-        {/* <div className="box nominal">
->>>>>>> ed393d9c94694451a22d3884576d46755ab4991f
-          <div className="box-form-data">
-            <div className="title-alt nomina">Nominal Top Up.</div>
-            <div className="topup-div">
-              <Link to="/topup">
-                <Button className="but-blue" style={butBlue}>
-                  TOP UP
-                </Button>
-              </Link>
-
-              <Button className="but" style={but}>
-                WITHDRAW
-              </Button>
-            </div>
-            <div className="input-border-underline inputan-uang">
-              <input type="number" name="username" />
-            </div>
-
-            <div className="geser button-saran-nih">{listSaran}</div>
-          </div>
-        </div> */}
-        {/* <div className="box riwayat">
-          <div className="title-alt riwayat-trans">Riwayat Transaksi</div>
-          <div className="menu">
-            <a
-              style={{
-                background: "#4CB5EF",
-                color: "white",
-              }}
-            >
-              Semua
-            </a>
-            <a className="satu-link">Pembelian</a>
-            <a className="dua-link">Top Up</a>
-            <a className="tiga-link">With Draw</a>
-          </div>
-
-          <div className="card-history">
-            <Card>
-              <p className="tanggal-now">10 Aug 2020 | 08:30</p>
-              <br />
-
-              <div className="title-alt no-trans">No Transaksi</div>
-              <p className="kode">(IVR/200804/XX/VII/469044911)</p>
-
-              <div className="Total" style={{ borderLeft: "" }}>
-                <div className="title-alt total-pemb">
-                  Total Pembayaran{" "}
-                  <span style={{ color: "#F27272" }}>Rp. 20.000.000</span>
-                </div>
-                <p className="lihat-detail-trans">Lihat Detail Transaksi</p>
-              </div>
-              <hr />
-              <div>
-                <div className="title-alt topup-yu">Top Up</div>
-                <img src={topup} />
-                <p className="nominal-topupx">+20.000.000</p>
-
-                <div className="status">
-                  <img />
-                  <div className="title-alt status-ku">Status</div>
-                  <p className="berhasil">Transaksi Berhasil</p>
-                </div>
-                <div className="pinggir-sikit">
-                  <Button className="but" style={but}>
-                    Top Up Lagi
-                  </Button>
-                </div>
-                <hr />
-              </div>
-
-              <p className="tanggal-now">12 Aug 2020 | 00:30</p>
-              <br />
-
-              <div className="title-alt no-trans">No Transaksi</div>
-              <p className="kode">(IVR/200804/XX/VII/469044911)</p>
-
-              <div className="Total">
-                <div className="title-alt total-pemb">
-                  Total Pembayaran{" "}
-                  <span style={{ color: "#F27272" }}>Rp. 20.000.000</span>
-                </div>
-                <p className="lihat-detail-trans">Lihat Detail Transaksi</p>
-              </div>
-              <hr />
-            </Card>
-            <div>
-              <div className="title-alt topup-yu">Top Up</div>
-              <img src={tarik} />
-              <p className="nominal-topupx">-20.000.000</p>
-
-              <div className="status">
-                <img />
-                <div className="title-alt status-ku">Status</div>
-                <p className="gagal">Transaksi Dibatalkan</p>
-              </div>
-              <div className="pinggir-sikit">
-                <Button className="but" style={but}>
-                  Tarik Uang Lagi
-                </Button>
-              </div>
-              <hr />
-            </div>
-          </div>
-        </div> */}
       </div>
     );
   }
